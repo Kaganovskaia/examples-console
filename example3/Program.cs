451973911a08a13450b42bb5ee374257ec8da573
IrinaKaganovskaia <Kaganovskaia@icloud.com>
2022-06-27 23:06:51 +0300

    Добавили Машу

---
+++ example3/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username == "Маша")
+{
+System.Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+System.Console.WriteLine("Привет, ");
+System.Console.WriteLine(username);
+}
