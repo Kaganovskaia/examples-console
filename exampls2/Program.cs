f6ec68dbc7bff7ffb15a0086a480f17f9a0e0300
IrinaKaganovskaia <Kaganovskaia@icloud.com>
2022-06-27 22:18:32 +0300

    Привет,юзер

---
+++ exampls2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+System.Console.Write("ВВедите ваше имя");
+string username = Console.ReadLine();
+System.Console.Write("привет,");
+System.Console.Write(username);
